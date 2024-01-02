e73babee8f2775039b05bd2dab41d0b859a5fe5b
zachary62 <zhuang333@wisc.edu>
2024-01-02 21:21:25 +0000

    update 1/2/2024

--- setup.py
+++ setup.py
@@ -2,9 +2,18 @@ from setuptools import setup, find_packages
 
 setup(
     name='cocoon_data',
-    version='0.1.4',
+    version='0.1.8',
     packages=find_packages(),
     install_requires=open('requirements.txt').read().splitlines(),
+    extras_require={
+        'geo': [
+            'geopandas==0.14.1', 
+            'rasterio',  
+            "pyproj",
+            "shapely",
+            "scipy"
+        ]
+    },
     author='Zachary Huang',
     author_email='zh2408@columbia.edu',
     description='Cocoon is an open-source project that aims to free analysts from tedious data transformations with LLM.',
