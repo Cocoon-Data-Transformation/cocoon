190c573b55ea6ec05c28057b92cdebccc01dacea
zachary62 <zhuang333@wisc.edu>
2024-02-02 15:57:21 +0000

    update cluster

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='cocoon_data',
-    version='0.1.12',
+    version='0.1.13',
     packages=find_packages(),
     install_requires=open('requirements.txt').read().splitlines(),
     extras_require={
