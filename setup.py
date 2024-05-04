2c76ac5a360720a9df0775cf61390ff9505c7c06
zachary62 <zhuang333@wisc.edu>
2024-05-04 21:10:47 +0000

    update data type“

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="cocoon_data",
-    version="0.1.41",
+    version="0.1.42",
     packages=find_packages(),
     install_requires=open("requirements.txt").read().splitlines(),
     extras_require={
