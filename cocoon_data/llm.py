79e744f469ad9fcb0b82e20b9a0d0e465d52b93d
zachary62 <zhuang333@wisc.edu>
2024-07-26 17:28:25 +0000

    hot fix

--- cocoon_data/llm.py
+++ cocoon_data/llm.py
@@ -1,151 +1,295 @@
-import re
-import yaml
-from collections import OrderedDict
-from collections import defaultdict
-import json
+import openai
 import os
+import json
+import hashlib
+from collections import OrderedDict
 
-def extract_json_code_safe(s):
-    s_stripped = s.strip()
-    if (s_stripped.startswith('{') and s_stripped.endswith('}')) or \
-       (s_stripped.startswith('[') and s_stripped.endswith(']')):
-        return s_stripped
+try:
+    import vertexai
+    from vertexai.preview.generative_models import GenerativeModel, Part, HarmCategory, HarmBlockThreshold
+except:
+    pass
 
-    json_code =  extract_json_code(s_stripped)
+try:
+    from anthropic import AnthropicBedrock
+except:
+    pass
 
-    if json_code is not None:
-        return json_code
+try:
+    import anthropic
+except:
+    pass
 
-    start_brace = s_stripped.find('{')
-    start_bracket = s_stripped.find('[')
-    start_index = start_brace if start_bracket == -1 else (start_bracket if start_brace == -1 else min(start_brace, start_bracket))
-
-    if start_index == -1:
-        return None
-
-    end_brace = s_stripped.rfind('}')
-    end_bracket = s_stripped.rfind(']')
-    end_index = end_brace if end_bracket == -1 else (end_bracket if end_brace == -1 else max(end_brace, end_bracket))
-
-    if end_index == -1:
-        return None
-
-    potential_json = s_stripped[start_index:end_index + 1]
-    return potential_json
-
-def extract_json_code(s):
-    import re
-    pattern = r"```json(.*?)```"
-    match = re.search(pattern, s, re.DOTALL)
-    return match.group(1).strip() if match else None
-
-def extract_python_code(s):
-    import re
-    pattern = r"```python(.*?)```"
-    match = re.search(pattern, s, re.DOTALL)
-    return match.group(1).strip() if match else None
-
-
-def extract_yml_code(s):
-    import re
-    pattern = r"```yml(.*?)```"
-    match = re.search(pattern, s, re.DOTALL)
-    return match.group(1).strip() if match else None
-
-
-def represent_ordereddict(dumper, data):
-    value = []
-    for item_key, item_value in data.items():
-        node_key = dumper.represent_data(item_key)
-        node_value = dumper.represent_data(item_value)
-        value.append((node_key, node_value))
-    return yaml.nodes.MappingNode(u'tag:yaml.org,2002:map', value)
-
-
-yaml.add_representer(OrderedDict, represent_ordereddict)
+try:
+    from anthropic import AnthropicVertex
+except:
+    pass
 
 
 
+if 'OPENAI_API_TYPE' in os.environ:
+    openai.api_type = os.environ['OPENAI_API_TYPE']
 
-def custom_constructor(loader, node):
-    value = loader.construct_scalar(node)
-    if value in ['true', 'True', 'TRUE']:
-        return True
-    elif value in ['false', 'False', 'FALSE']:
-        return False
-    return str(value)
+if 'OPENAI_API_BASE' in os.environ:
+    openai.api_base = os.environ['OPENAI_API_BASE']
 
-yaml.SafeLoader.add_constructor('tag:yaml.org,2002:bool', custom_constructor)
-yaml.SafeLoader.add_constructor('tag:yaml.org,2002:str', custom_constructor)
+if 'OPENAI_API_KEY' in os.environ:
+    openai.api_key = os.environ['OPENAI_API_KEY']
 
+if 'OPENAI_API_VERSION' in os.environ:
+    openai.api_version = os.environ['OPENAI_API_VERSION']
 
+if 'OPENAI_GPT4_ENGINE' in os.environ:
+    openai.gpt4_engine = os.environ['OPENAI_GPT4_ENGINE']
 
-
-
-def is_test_node(node_id, node_info):
-    return node_info.get('resource_type') == 'test'
-
-def build_lineage_graph(dbt_path, manifest_path=None, catalog_path=None):
-    if manifest_path is None:
-        manifest_path = os.path.join(dbt_path, 'target', 'manifest.json')
-    if catalog_path is None:
-        catalog_path = os.path.join(dbt_path, 'docs', 'catalog.json')
+if 'OPENAI_EMBED_ENGINE' in os.environ:
+    openai.embed_engine = os.environ['OPENAI_EMBED_ENGINE']
     
-    if not os.path.exists(manifest_path):
-        raise FileNotFoundError(f"Manifest file not found at {manifest_path}. Have you run 'dbt docs generate'? You can also specify the path to the manifest file using the 'manifest_path' argument.")
-    if not os.path.exists(catalog_path):
-        raise FileNotFoundError(f"Catalog file not found at {catalog_path}. Have you run 'dbt docs generate'? You can also specify the path to the catalog file using the 'catalog_path' argument.")
+cocoon_llm_setting = {
+    'aws_access_key': os.environ.get("AWS_ACCESS_KEY_ID", None),
+    'aws_secret_key': os.environ.get("AWS_SECRET_ACCESS_KEY", None),
+    'aws_region': os.environ.get("AWS_REGION", None),
+    'aws_model': os.environ.get("AWS_MODEL", "anthropic.claude-3-5-sonnet-20240620-v1:0"),
+    'claude_model': os.environ.get("CLAUDE_MODEL", "claude-3-5-sonnet-20240620"),
+    'vertex_model': os.environ.get("VERTEX_MODEL", "claude-3-5-sonnet@20240620"),
+    'vertex_region': os.environ.get("AnthropicVertex_region", None),
+    'vertex_project_id': os.environ.get("AnthropicVertex_project_id", None),
+    'openai_model': os.environ.get("OPENAI_MODEL", "gpt-4-turbo"),
+}
+    
 
-    with open(manifest_path, 'r') as f:
-        manifest = json.load(f)
+def call_embed(input_string):
+
+    if openai.api_type == 'azure':
+        response = openai.Embedding.create(input=input_string, 
+            engine=openai.embed_engine)
+        return response
     
-    with open(catalog_path, 'r') as f:
-        catalog = json.load(f)
-    
-    nodes = set()
-    dependencies = defaultdict(set)
-    sql_content_mapping = {}
-    column_mapping = {}
-    
-    for node_id, node_info in manifest['nodes'].items():
-        if not is_test_node(node_id, node_info):
-            nodes.add(node_id)
-            
-            compiled_path = node_info.get('compiled_path')
-            if compiled_path:
-                full_sql_path = os.path.join(dbt_path, compiled_path)
-                if os.path.exists(full_sql_path):
-                    try:
-                        with open(full_sql_path, 'r') as sql_file:
-                            sql_content = sql_file.read()
-                        sql_content_mapping[node_id] = sql_content
-                    except Exception as e:
-                        print(f"Error reading SQL file for {node_id}: {str(e)}")
-                else:
-                    print(f"Compiled SQL file not found at {full_sql_path}")
-            
-            if node_id in catalog['nodes']:
-                column_mapping[node_id] = catalog['nodes'][node_id]['columns']
-            else:
-                print(f"Node {node_id} not found in catalog")
-            
-            if 'depends_on' in node_info and 'nodes' in node_info['depends_on']:
-                for dep in node_info['depends_on']['nodes']:
-                    dep_info = manifest['nodes'].get(dep, {})
-                    if not is_test_node(dep, dep_info):
-                        dependencies[node_id].add(dep)
-                        nodes.add(dep)
-    
-    nodes_list = sorted(list(nodes))
-    node_to_index = {node: index for index, node in enumerate(nodes_list)}
-    
-    edges = []
-    for node, deps in dependencies.items():
-        for dep in deps:
-            edges.append((node_to_index[dep], node_to_index[node]))
-    
-    return nodes_list, edges, sql_content_mapping, column_mapping
+    elif openai.api_type == 'open_ai':
+        response = openai.Embedding.create(
+            model="text-embedding-ada-002",
+            input=input_string
+        )
+        return response
+
+def hash_messages(messages):
+    serialized_input = json.dumps(messages, sort_keys=True)
+    return hashlib.sha256(serialized_input.encode('utf-8')).hexdigest()
+
+class LRUCache:
+    def __init__(self, capacity: int = 100):
+        self.cache = OrderedDict()
+        self.capacity = capacity
+
+    def get(self, key: str):
+        if key not in self.cache:
+            return None
+        else:
+            self.cache.move_to_end(key)
+            return self.cache[key]
+
+    def put(self, key: str, value):
+        if key in self.cache:
+            self.cache.move_to_end(key)
+        else:
+            if len(self.cache) >= self.capacity:
+                self.cache.popitem(last=False)
+        self.cache[key] = value
+
+    def pop(self):
+        self.cache.popitem()
+
+    def save_to_disk(self, file_path = "./cached_messages.json"):
+        with open(file_path, 'w') as file:
+            json.dump(dict(self.cache), file)
+
+    def load_from_disk(self, file_path= "./cached_messages.json"):
+        with open(file_path, 'r') as file:
+            loaded_cache = json.load(file)
+            self.cache = OrderedDict(loaded_cache)
+
+lru_cache = LRUCache(5000)
 
 
 
 
+
+
+
+if os.path.exists("./cached_messages.json"):
+    lru_cache.load_from_disk("./cached_messages.json")
+
+def call_llm_chat(messages, temperature=0.1, top_p=0.1, use_cache=True):
+
+    message_hash = hash_messages(messages)
+
+    if use_cache:
+        response = lru_cache.get(message_hash)
+        if response is not None:
+            lru_cache.save_to_disk()
+            return response
+
+    if openai.api_type == 'Anthropic':
+        messages = convert_openai_to_claude(messages)
+
+        client = anthropic.Anthropic()
+        responses = client.messages.create(
+            model=cocoon_llm_setting['claude_model'] or os.environ.get("CLAUDE_MODEL"),
+            max_tokens=4000,
+            messages=messages
+        )
+
+        response = convert_claude_to_openai(responses)
+        
+    elif openai.api_type == 'AnthropicBedrock':
+        messages = convert_openai_to_claude(messages)
+
+        client = AnthropicBedrock(
+            aws_access_key=cocoon_llm_setting['aws_access_key'] or os.environ.get("AWS_ACCESS_KEY_ID"),
+            aws_secret_key=cocoon_llm_setting['aws_secret_key'] or os.environ.get("AWS_SECRET_ACCESS_KEY"),
+            aws_region=cocoon_llm_setting['aws_region'] or os.environ.get("AWS_REGION") or "us-east-1",
+        )
+
+        message = client.messages.create(
+            model=cocoon_llm_setting['aws_model'] or os.environ.get("AWS_MODEL"),
+            max_tokens=4000,
+            messages=messages
+        )
+
+        response = convert_anthropicvertex_to_openai(message)
+        
+    elif openai.api_type == 'AnthropicVertex':
+        messages = convert_openai_to_claude(messages)
+
+        client = AnthropicVertex(region=cocoon_llm_setting['vertex_region'] or os.environ.get("AnthropicVertex_region"),
+                                 project_id=cocoon_llm_setting['vertex_project_id'] or os.environ.get("AnthropicVertex_project_id"))
+        
+        responses = client.messages.create(
+            max_tokens=4000,
+            messages=messages,
+            model=cocoon_llm_setting['vertex_model'] or os.environ.get("VERTEX_MODEL"),
+            )
+
+        response = convert_anthropicvertex_to_openai(responses)
+        
+    elif openai.api_type == 'gemini':
+        messages = convert_openai_to_gemini(messages)
+
+        model = GenerativeModel("gemini-pro-vision")
+
+        safety_settings={
+            HarmCategory.HARM_CATEGORY_HARASSMENT: HarmBlockThreshold.BLOCK_NONE,
+            HarmCategory.HARM_CATEGORY_HATE_SPEECH: HarmBlockThreshold.BLOCK_NONE,
+            HarmCategory.HARM_CATEGORY_SEXUALLY_EXPLICIT: HarmBlockThreshold.BLOCK_NONE,
+            HarmCategory.HARM_CATEGORY_DANGEROUS_CONTENT: HarmBlockThreshold.BLOCK_NONE,
+        }
+                
+        responses = model.generate_content(
+            messages,
+            generation_config={
+                "max_output_tokens": 2048,
+                "temperature": 0.4,
+                "top_p": 1,
+                "top_k": 32
+            },
+            safety_settings=safety_settings
+        )
+
+        response = convert_gemini_to_openai(responses)
+
+    elif openai.api_type == 'azure':
+        response = openai.ChatCompletion.create(
+            engine = openai.gpt4_engine,
+            temperature=temperature,
+            top_p=top_p,
+            messages=messages
+        )
+        
+    elif openai.api_type == 'open_ai':
+        
+        response = openai.ChatCompletion.create(
+            model="gpt-4-turbo",
+            temperature=temperature,
+            top_p=top_p,
+            messages=messages
+        )
+        
+    
+    else:
+        raise ValueError(f"openai.api_type is {openai.api_type}, but it should be 'azure' or 'openai'.")
+
+    lru_cache.put(message_hash, response)
+    lru_cache.save_to_disk()
+    return response
+
+
+
+
+
+
+
+
+def convert_openai_to_gemini(openai_messages):
+    gemini_messages = []
+
+    for message in openai_messages:
+        role = "USER" if message["role"] == "user" else "model"
+
+        gemini_message = {
+            "role": role.upper(),
+            "parts": [{"text": message["content"]}]
+        }
+
+        gemini_messages.append(gemini_message)
+
+    return gemini_messages
+
+
+
+
+def convert_gemini_to_openai(gemini_response):
+    message_text = gemini_response.candidates[0].content.parts[0].text
+
+    message_content = {"role": "assistant", "content": message_text}
+
+    openai_response = {
+        "choices": [
+            {"message": message_content}
+        ]
+    }
+
+    return openai_response
+
+
+
+
+def convert_anthropicvertex_to_openai(anthropicvertex_response):
+    message_text = anthropicvertex_response.content[0].text
+
+    message_content = {"role": "assistant", "content": message_text}
+
+    openai_response = {
+        "choices": [
+            {"message": message_content}
+        ]
+    }
+
+    return openai_response
+
+
+def convert_openai_to_claude(openai_messages):
+    return openai_messages
+
+
+
+
+def convert_claude_to_openai(claude_response):
+    message_text = claude_response.content[0].text
+
+    openai_response = {
+        "choices": [
+            {"message": {"role": "assistant", "content": message_text}}
+        ]
+    }
+
+    return openai_response
